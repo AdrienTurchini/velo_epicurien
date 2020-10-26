55890d4dd49bc6892ef81362f07db62aaf498a18
Adrien Turchini <adrien.turchini@me.com>
2020-10-26 11:13:19 +0100

    add scraping

---
+++ scraping/server/sandboxtrip.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-console, no-process-exit */
+const trip = require('./tripadvisor')
+
+trip_hp = 'https://www.tripadvisor.fr/Restaurants-g155033-Quebec_City_Quebec.html';
+
+var fs = require('fs');
+
+async function sandbox() {
+
+  try {
+    console.log(`🕵️‍♀️  browsing TripAdvisor source, please wait !`);
+
+    // get number of pages for quebec's tripadvisor restaurants 
+    const nbpage = await trip.scrapeFirstSearchPage(trip_hp);
+
+    // get a list of the url of restaurants
+    var linksRestau = [];
+    for(var i = 0; i < nbpage; i++){
+      const links = await trip.scrapeASearchPage('https://www.tripadvisor.fr/RestaurantSearch?Action=PAGE&ajax=1&availSearchEnabled=false&sortOrder=popularity&geo=155033&itags=10591&o=a' + String(i*30));
+      links.forEach(element => linksRestau.push(element));
+    }
+
+    // get a list of informations for all  restaurants
+    var listOfRestauDetails = [];  
+    for(var i = 0; i < linksRestau.length; i++){
+      var restaurant = await trip.scrapeRestaurant('https://www.tripadvisor.fr'+linksRestau[i]);
+      restaurant = {name: restaurant.name, classement: restaurant.classement, address: restaurant.address, link: 'https://www.tripadvisor.fr'+linksRestau[i], geometry: restaurant.geo};
+      listOfRestauDetails.push(restaurant);
+      console.log(i);
+    }
+
+    // write data in a json file
+    var fs = require('fs');
+    fs.writeFileSync('./quebec_restaurants.json', JSON.stringify(listOfRestauDetails, null, 4), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      };
+      console.log("File has been created");
+    });
+
+    console.log(nbpage, linksRestau.length, listOfRestauDetails.length);
+    
+
+    process.exit(0);
+  } catch (e) {
+    console.error(e); 
+    process.exit(1);
+  }
+}
+const [, , searchLink] = process.argv;
+sandbox(searchLink);
---
+++ scraping/server/tripadvisor.js
@@ -0,0 +1,102 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+// return number of pages containing restaurants 
+const firstPage = data => {
+  const $ = cheerio.load(data);
+
+  // num of restaurants
+  var num_restau = $('#EATERY_LIST_CONTENTS > div.deckTools.btm > div > div > a:nth-child(8)').text();
+  return num_restau;
+}
+
+// get links from search page
+const searchPage = data => {
+  const $ = cheerio.load(data);
+  var links = [];
+  // get all the restaurant links for one page
+  $("#component_2 > div > div._1llCuDZj > span > div._1kNOY9zw > div._2Q7zqOgW > div._2kbTRHSI > div > span > a").each((i, elem) => {
+    links.push($(elem).prop("href"));
+  });
+  return links;
+}
+
+/**
+ * Parse webpage restaurant
+ * @param  {String} data - html response
+ * @return {Object} restaurant
+ */
+// return restaurants details
+const restauDetails = data => {
+  const $ = cheerio.load(data);
+
+  // get name
+  var name = $('._3a1XQ88S').text();
+
+  // get classement
+  var classement = $('.r2Cf69qf').text();
+  classement = classement.substr(0,3);
+ 
+  // get address
+  var address = $('._2saB_OSe').first().text();
+
+  // get coordinates
+ var text =  $('#taplc_footer_js_globals_0 > script:nth-child(6)')[0].children[0].data;
+ index_coords_false = text.indexOf("\"coords\":\"");
+ index_coords = text.indexOf("\"coords\":\"",index_coords_false + 29);
+ geo_normal = text.substr(index_coords+10, 19);
+ geo_normal = geo_normal.replace(/[^0-9.,-]/g, "")
+ long_lat = geo_normal.split(",");
+ geo_inverted = [parseFloat(long_lat[1]),parseFloat(long_lat[0])];
+ geo = {"type" : "Point", "coordinates":geo_inverted}
+
+
+ return { name, classement, address, geo};
+};
+
+// return links of restaurants for one page
+module.exports.scrapeASearchPage = async url => {
+  const response = await axios(url);
+  const { data, status } = response;
+  if (status >= 200 && status < 300) {
+    return searchPage(data);
+  }
+  console.error(status);
+  return null;
+};
+
+// return number of pages to scrap
+module.exports.scrapeFirstSearchPage = async url => {
+  const response = await axios(url);
+  const { data, status } = response;
+  if (status >= 200 && status < 300) {
+    return firstPage(data);
+  }
+  console.error(status);
+  return null;
+};
+
+/**
+ * Scrape a given restaurant url
+ * @param  {String}  url
+ * @return {Object} restaurant
+ */
+// return restaurants details
+module.exports.scrapeRestaurant = async url => {
+  const response = await axios(url);
+  const { data, status } = response;
+  if (status >= 200 && status < 300) {
+    return restauDetails(data);
+  }
+  console.error(status);
+  return null;
+};
+
+/**
+ * Get all restaurants
+ * @return {Array} restaurants
+ */
+module.exports.get = () => {
+  return [];
+};
